50619e06f4def6ce93bb962d2b384ae32e0a7a3d
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-09 13:08:51 +0100

    added form submit & increase, decrease buttons

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,24 @@
 $('.carousel').carousel({
     interval: false,
 });
+
+
+// increase brightness
+
+function increaseBrightness() {
+    var element = document.getElementById("scrn");
+    element.classList.add("increaseBrightness");
+    element.classList.remove("decreaseBrightness");
+}
+
+function decreaseBrightness() {
+    var element = document.getElementById("scrn");
+    element.classList.add("decreaseBrightness");
+    element.classList.remove("increaseBrightness");
+}
+
+
+/// grabbing keywords
+function grabKeyword(){
+    var element = document.getElementById("#smtg")
+}
