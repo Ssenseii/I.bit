b1f6659ae6318b0f1e00131211da098c692c8f62
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-19 12:26:25 +0100

    added some more to item 5

--- app/js/script.js
+++ app/js/script.js
@@ -19,6 +19,24 @@ $('.carousel').carousel({
     interval: false,
 });
 
+///collapsible
+
+var coll = document.getElementsByClassName("item__five-header-actions-settings");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("circleActive");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+
 
 // increase brightness
 
