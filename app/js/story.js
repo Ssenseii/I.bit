10981aefc92017200818692e2051de9c5b23626b
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-21 13:43:39 +0100

    finished the burn spot

--- app/js/story.js
+++ app/js/story.js
@@ -134,12 +134,12 @@ if (c1.addEventListener("click", function () {
 
 /// Burn Location
 
-if (c1.addEventListener("click", function () {
+if (c2.addEventListener("click", function () {
     story.innerHTML = "You believe the cops have done a terrible job in investigating the murder of your friend. <br> You believe they left a ton of evidence unaccounted for.<br> When you get there. The smell of burnt flesh is still in the air. <br> drops of blood are scattered down a tree bark. <br> You try to do some investigative work";
 
     c1.innerHTML = "Inspect the surrounding grounds";
     c2.innerHTML = "Inspect the river";
-    c3.innerHTML = "Inspect the ash covered fround";
+    c3.innerHTML = "Inspect the ash covered ground";
 
     /// Surrounding Ground
     if (c1.addEventListener("click", function () {
@@ -166,7 +166,7 @@ if (c1.addEventListener("click", function () {
         }));
 
         if (c3.addEventListener("click", function () {
-            story.innerHTML = "You die for eating a sandwhich.";
+            story.innerHTML = "You die eating a sandwhich.";
 
             c1.style.display = "none";
             c2.style.display = "none";
@@ -179,15 +179,15 @@ if (c1.addEventListener("click", function () {
     /// the river
 
     if (c2.addEventListener("click", function () {
-        story.innerHTML = "Water runs";
-        /// you stopped here
+        story.innerHTML = "Water flows. You can't help but wonder if her blood ran through with the stream. <br> <br> It doesn't seem as interesting";
+        
 
-        c1.innerHTML = "Look through the microscope at the mushrooms";
-        c2.innerHTML = "Steal the mushrooms";
-        c3.innerHTML = "Eat the mushrooms";
+        c1.innerHTML = "Follow the river";
+        c2.innerHTML = "Inspect the grounds around the river";
+        c3.innerHTML = "try crossing the river to the other side";
 
         if (c1.addEventListener("click", function () {
-            story.innerHTML = "The light from the mushrooms bursts through the lens and blinds you. You forgot to wear eye protective gear. <br>. You take a few steps back and fall on your head. <br> You are blind and dead."
+            story.innerHTML = "The river keeps on going, and you walk following it. until you reach a rock in the middle of the stream, with a rope stuck in one of its crevices, it's other end lasso'd to some sort of a metal can. You try to lean in and grab the rope... You die."
 
             c1.style.display = "none";
             c2.style.display = "none";
@@ -195,7 +195,7 @@ if (c1.addEventListener("click", function () {
         }));
 
         if (c2.addEventListener("click", function () {
-            story.innerHTML = "You stole the mushrooms.<br> You run away and sold them to the competition. <br> You received millions of dollars as a reward and became very very rich. <br> you forgot about your friend.<br> you are and always will be an asshole."
+            story.innerHTML = "stacy's footmarks, facing the river. <br> There's a deep mark too. <br> Someone jumped from here. <br><br> You imitate the jump and you drown 3 minutes later."
 
             c1.style.display = "none";
             c2.style.display = "none";
@@ -203,7 +203,7 @@ if (c1.addEventListener("click", function () {
         }));
 
         if (c3.addEventListener("click", function () {
-            story.innerHTML = 'The mushrooms melt in your tongue. <br> You fart and the air materializes into a duplicate of you. <br> You have created a clone. The clone speaks only one word <span style="color: #ff1100;" >Blaze</span>';
+            story.innerHTML = "Have you not learned from the previous two choices? <br> I end up coding your death myself for being an idiot";
 
             c1.style.display = "none";
             c2.style.display = "none";
@@ -213,16 +213,16 @@ if (c1.addEventListener("click", function () {
 
     }));
 
-    /// Hiding
+    /// Ash covered ground
     if (c3.addEventListener("click", function () {
-        story.innerHTML = "Initially, You notice that someone has been sleeping on the couch recently, There is warm food on the desk next to the unfinished note.";
+        story.innerHTML = "Ah dear Stacy, She was burned here. black marks all around. <br> She must've went up in flames. which means she must've been covered in a fire instigator like gasoline.";
 
-        c1.innerHTML = "Check the couch";
-        c2.innerHTML = "Inspect the food";
+        c1.innerHTML = "Look for a gas tank";
+        c2.innerHTML = "Inspect the ash";
         c3.style.display = "none";
 
         if (c1.addEventListener("click", function () {
-            story.innerHTML = "That's weird. The couch still smells like Stacy, and there is a giant hole in the middle as if someone was reently sleeping on it. <br> You get hit in the back of the head. <br> You are unconscious and soon to be dead."
+            story.innerHTML = "You keep looking and looking around but you can't find anything. Maybe whoever burned her threw the can in the river."
 
             c1.style.display = "none";
             c2.style.display = "none";
@@ -230,7 +230,7 @@ if (c1.addEventListener("click", function () {
         }));
 
         if (c2.addEventListener("click", function () {
-            story.innerHTML = "My love, Jeremy... <br> I can't rememeber the last time you and I have been together, I miss you dearly and ~ <br> You are stabbed in the back by someone you can't see in the dark. <br> You are dead."
+            story.innerHTML = "It's odd, you've never seen ash like this before. You can almost make out its faint glow."
 
             c1.style.display = "none";
             c2.style.display = "none";
@@ -241,3 +241,128 @@ if (c1.addEventListener("click", function () {
 
 
 }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (c3.addEventListener("click", function () {
+    story.innerHTML = "Stacy's House in a mess, You can't believe a scientist like her lives like this. <br> There' s a horrible smell in her bathroom. <br> There's a sound coming out of the pantry, like scratching. <br> Something is giving light in the bedroom, and you believe, you just heard someone laugh ";
+
+    c1.innerHTML = "Sneak into the bathroom";
+    c2.innerHTML = "Open the pantry door";
+    c3.innerHTML = "Sneak into the bedroom and see who it is";
+
+    /// Bathroom
+
+    if (c1.addEventListener("click", function () {
+        story.innerHTML = "The horrible smell is coming from the closed toilet seat, Someone is showering behind the curtains with the lights is off. <br> unrelated: you feel a fart coming";
+
+        c1.innerHTML = "Open the toilet seat";
+        c2.innerHTML = "See who's showering";
+        c3.innerHTML = "Fart";
+
+        if (c1.addEventListener("click", function () {
+            story.innerHTML = "That's stacy's decomposing head, green blood."
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+        if (c2.addEventListener("click", function () {
+            story.innerHTML = "That's stacy showering. But she isn't the same Stacy, something is off about her."
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+        if (c3.addEventListener("click", function () {
+            story.innerHTML = "The entire house clicks their tongue synchronically. You alerted the hive of Stacys";
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+
+    }));
+
+    /// Pantry
+
+    if (c2.addEventListener("click", function () {
+        story.innerHTML = "You open the pantry door. The same spider from the computer video is crawling around ";
+        
+/// stopped here
+        c1.innerHTML = "Follow the river";
+        c2.innerHTML = "Inspect the grounds around the river";
+        c3.innerHTML = "try crossing the river to the other side";
+
+        if (c1.addEventListener("click", function () {
+            story.innerHTML = "The river keeps on going, and you walk following it. until you reach a rock in the middle of the stream, with a rope stuck in one of its crevices, it's other end lasso'd to some sort of a metal can. You try to lean in and grab the rope... You die."
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+        if (c2.addEventListener("click", function () {
+            story.innerHTML = "stacy's footmarks, facing the river. <br> There's a deep mark too. <br> Someone jumped from here. <br><br> You imitate the jump and you drown 3 minutes later."
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+        if (c3.addEventListener("click", function () {
+            story.innerHTML = "Have you not learned from the previous two choices? <br> I end up coding your death myself for being an idiot";
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+
+    }));
+
+    /// Bedroom
+    if (c3.addEventListener("click", function () {
+        story.innerHTML = "Ah dear Stacy, She was burned here. black marks all around. <br> She must've went up in flames. which means she must've been covered in a fire instigator like gasoline.";
+
+        c1.innerHTML = "Look for a gas tank";
+        c2.innerHTML = "Inspect the ash";
+        c3.style.display = "none";
+
+        if (c1.addEventListener("click", function () {
+            story.innerHTML = "You keep looking and looking around but you can't find anything. Maybe whoever burned her threw the can in the river."
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+        if (c2.addEventListener("click", function () {
+            story.innerHTML = "It's odd, you've never seen ash like this before. You can almost make out its faint glow."
+
+            c1.style.display = "none";
+            c2.style.display = "none";
+            c3.style.display = "none";
+        }));
+
+    }));
+
+
+}));
+
+
+
